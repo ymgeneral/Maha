a813433c9b0694b4ebaf3d439ee0840d1c53f22e
YM <ymgeneral@live.com>
2018-10-28 15:27:30 +0800

    添加host dotnetcore

---
+++ Maha.Microservices.HostCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Maha.Microservices.HostCore.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
