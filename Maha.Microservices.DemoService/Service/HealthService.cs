070de7735516fa312c4af1adce0a91beaa764823
JasonChan <504433707@qq.com>
2018-12-08 13:44:32 +0800

    当前可使用json rpc版本

---
+++ Maha.Microservices.DemoService/Service/HealthService.cs
@@ -0,0 +1,17 @@
+﻿namespace Maha.Microservices.DemoService
+{
+    /// <summary>
+    /// 健康检查服务
+    /// </summary>
+    public class HealthService
+    {
+        /// <summary>
+        /// consul 心跳健康检查
+        /// </summary>
+        /// <returns></returns>
+        public string check()
+        {
+            return "ok";
+        }
+    }
+}
